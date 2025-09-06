ef63f38bca3663f53fd357ae2de3629b3d4e6fcb
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2025-09-06 10:13:33 -0400

    Update __init__.py

--- FastAudioSR/__init__.py
+++ FastAudioSR/__init__.py
@@ -1,5 +1,4 @@
 import torch
-import torchaudio
 import os
 from scipy.io.wavfile import write
 from .speechsr import SynthesizerTrn
@@ -39,15 +38,6 @@ class FASR:
         model.eval()
         return model
 
-
-    def load_and_preprocess_audio(self, input_path):
-        audio, sample_rate = torchaudio.load(input_path)
-        audio = audio[:1, :]
-        if sample_rate != 16000:
-            audio = torchaudio.functional.resample(audio, sample_rate, 16000, resampling_method="kaiser_window")
-        return audio
-
-
     def super_resolution(self, audio):
         with torch.no_grad():
             converted_audio = self.model(audio.unsqueeze(1).to(self.device))
@@ -62,9 +52,7 @@ class FASR:
         write(output_path, 48000, audio_numpy)
 
 
-    def run(self, input_speech, output_path):
-        input_audio = self.load_and_preprocess_audio(input_speech)
+    def run(self, input_speech):
+        input_audio = torch.from_numpy(input_speech)
         output_audio = self.super_resolution(input_audio)
-        #audio_numpy = (audio.numpy() * 32767.0).astype('int16')
         return output_audio
-        #self.save_audio(output_audio, output_path)
